f0c3ad88af36f4e0f3a0ebc16625642ebe31c580
user <user@ZC415014>
2023-01-13 08:57:43 +0200

    final

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace exe_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //declaring variables
+            int length = 0, width = 0;
+
+            //recieving width and length
+            Console.WriteLine("Please enter quadrangular length:");
+            length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter quadrangular width:");
+            width = int.Parse(Console.ReadLine());
+
+            //if measures are equal print that is is a square
+            if (length == width)
+            {
+                Console.WriteLine("The quadrangular is a square");
+            }
+            //if not print difference between measures
+            else
+            {
+                Console.WriteLine($"The difference between the length and the width is {length-width}");
+            }
         }
     }
 }
